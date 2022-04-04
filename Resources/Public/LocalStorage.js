3772bd8294771cf95da63eb25f680df8c261ceec
Jon Uhlmann <account@uhlmann.pro>
2022-04-04 18:13:41 +0200

    Upgrade: Packages

--- Resources/Public/LocalStorage.js
+++ Resources/Public/LocalStorage.js
@@ -1,1 +1,1 @@
-(()=>{var t=()=>localStorage.plausible_ignore==="true",l=()=>localStorage.plausible_ignore=!0;var e=()=>{let o=t()?0:5e3;l(),setTimeout(()=>{window.location="/"},o)};e();})();
+(()=>{var t=()=>localStorage.plausible_ignore==="true",l=()=>localStorage.plausible_ignore=!0;var o=()=>{let e=t()?0:5e3;l(),setTimeout(()=>{window.location="/"},e)};o();})();
