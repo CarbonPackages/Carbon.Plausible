4021f85fac86578d9b9904b9984bdb39b92ebf14
Jon Uhlmann <account@uhlmann.pro>
2023-10-19 15:28:11 +0200

    New: Add pageviewProps, taggedEvents and revenue option

--- Classes/EelHelper/PlausibleHelper.php
+++ Classes/EelHelper/PlausibleHelper.php
@@ -8,7 +8,6 @@ use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Http\Helper\RequestInformationHelper;
 use Psr\Http\Message\ServerRequestInterface;
 
-
 /**
  * @Flow\Proxy(false)
  */
@@ -64,10 +63,96 @@ class PlausibleHelper implements ProtectedContextAwareInterface
     }
 
     /**
+     * Get the value of a key from the site settings or the default settings
+     *
+     * @param array $defaultSettings
+     * @param array $siteSettings
+     * @param string $key
+     * @return bool
+     */
+    public function getBooleanValue(array $defaultSettings, array $siteSettings, string $key): bool
+    {
+        return !!$this->getValue($defaultSettings, $siteSettings, $key);
+    }
+
+    /**
+     * Get the value of a key from the site settings or the default settings
+     *
+     * @param array $defaultSettings
+     * @param array $siteSettings
+     * @param string $key
+     * @return mixed
+     */
+    public function getValue(array $defaultSettings, array $siteSettings, string $key)
+    {
+        if (isset($siteSettings[$key])) {
+            return $siteSettings[$key];
+        }
+        if (isset($defaultSettings[$key])) {
+            return $defaultSettings[$key];
+        }
+        return false;
+    }
+
+    /**
+     * Iterate over the given array and return a string with the key and value
+     *
+     * @param array $variable
+     * @return array
+     */
+    public function pageviewProps(array $variable): array {
+        $result = [];
+        foreach ($variable as $key => $value) {
+            $key = $this->getKeyForPageviewProps($key);
+            $value = $this->toString($value);
+            $result[$key] = $value;
+        }
+        return array_unique($result);
+    }
+
+    /**
+     * Convert the given key to a string
+     *
+     * @param string $string
+     * @return string
+     */
+    private function getKeyForPageviewProps(string $string): string {
+        $separator = '_';
+        $string = strtolower(
+            preg_replace(
+                '/([a-zA-Z])(?=[A-Z])/',
+                '$1' . $separator,
+                $string
+            )
+        );
+        return 'event-' . $string;
+    }
+
+    /**
+     * Convert the given value to a string
+     *
+     * @param mixed $value The value to convert (must be convertible to string)
+     * @return string The string value
+     */
+    private function toString($value): string
+    {
+        if (is_bool($value)) {
+            return $value ? 'true' : 'false';
+        }
+        if (is_array($value)) {
+            return implode(',', $value);
+        }
+        return (string)$value;
+    }
+
+
+    //  reverseProxy = ${Type.isBoolean(this.siteSettings.reverseProxy) ? this.siteSettings.reverseProxy : this.defaultSettings.reverseProxy}
+
+    /**
      * All methods are considered safe
-     * 
+     *
      * @param string $methodName The name of the method
-     * 
+     *
      * @return bool
      */
     public function allowsCallOfMethod($methodName)
